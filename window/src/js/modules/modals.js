8f4f4c61bb7ffff005b4879bfebba71dec702479
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2021-12-26 13:40:33 +0200

    added modal with setTimeout adn refactoring func bindModal

--- window/src/js/modules/modals.js
+++ window/src/js/modules/modals.js
@@ -1,45 +1,69 @@
 const modals = () => {
   // trigger - it's button or link
-  const bindModal = (trigger, modal, close) => {
-    //OPEN modal window
-    trigger.addEventListener('click', (e) => {
-      if (e.target) {
-        e.preventDefault();
-      }
+  const bindModal = (triggerSelector, modalSelector, closeSelector) => {
+    //trigger - pseudo array selectors
+    const trigger = document.querySelectorAll(triggerSelector);
+    const modal = document.querySelector(modalSelector);
+    const close = document.querySelector(closeSelector);
 
-      modal.style.display = 'block';
-      //when open modal - scroll from right side - hide
-      // document.body.style.overflow = 'hidden';
-      //add class bootstrap
-      document.body.classList.add('modal-open');
+    //pseudo array selectors
+    trigger.forEach((item) => {
+      //OPEN modal window
+      item.addEventListener('click', (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
+
+        modal.style.display = 'block';
+        // //when open modal - scroll from right side - hide
+        document.body.style.overflow = 'hidden';
+        // //open modal window - with add bootstrap class
+        // document.body.classList.add('modal-open');
+      });
     });
 
     //CLOSE modal window
     close.addEventListener('click', () => {
       modal.style.display = 'none';
-      // document.body.style.overflow = '';
-      //remove class bootstrap
-      document.body.classList.remove('modal-open');
+      document.body.style.overflow = '';
+      //close modal window - with remove bootstrap class
+      // document.body.classList.remove('modal-open');
     });
 
     //CLOSE modal window when click outside modal window
     modal.addEventListener('click', (e) => {
       if (e.target === modal) {
         modal.style.display = 'none';
-        // document.body.style.overflow = '';
-        //remove class bootstrap
-        document.body.classList.remove('modal-open');
+        document.body.style.overflow = '';
+        //close modal window - with remove bootstrap class
+        // document.body.classList.remove('modal-open');
       }
     });
   };
 
-  const callEngineerBtn = document.querySelector('.popup_engineer_btn');
-  const modalEngineer = document.querySelector('.popup_engineer');
-  const modalEngineerClose = document.querySelector(
+  const showModalByTime = (targetSelector, time) => {
+    setTimeout(() => {
+      document.querySelector(targetSelector).style.display = 'block';
+      document.body.style.overflow = '';
+    }, time * 1000);
+  };
+
+  // const callEngineerBtn = document.querySelector('.popup_engineer_btn');
+  // const modalEngineer = document.querySelector('.popup_engineer');
+  // const modalEngineerClose = document.querySelector(
+  //   '.popup_engineer .popup_close'
+  // );
+
+  //run by button
+  bindModal(
+    '.popup_engineer_btn',
+    '.popup_engineer',
     '.popup_engineer .popup_close'
   );
+  //run by link
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
 
-  bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+  showModalByTime('.popup', 3);
 };
 
 export default modals;
